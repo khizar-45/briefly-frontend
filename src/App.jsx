54ae274d76d1076baad22798d6e1992b9617b9cf
khizar-45 <skkhizarali45@gmail.com>
2025-08-01 13:29:31 +0530

    changed buttons ui and added 2 fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="min-h-screen w-full flex flex-col bg-black text-white overflow-x-hidden">
+    <div className="h-[100dvh] w-full flex flex-col bg-black text-white overflow-x-hidden">
       <nav className="fixed top-0 left-0 h-26 w-full flex gap-4 justify-center items-center px-10 py-4 bg-black/80 backdrop-blur-lg border-b border-white/20">
         <div className="flex gap-4 items-center">
           <img
@@ -12,11 +12,11 @@ function App() {
         </div>
       </nav>
 
-      <main className="pt-26 w-full flex-1 flex flex-col justify-center items-center text-center px-4">
+      <main className="pt-26 w-full flex-1 flex flex-col justify-center items-center text-center px-4 mx-auto">
         <img
           src="/brieflyLogoPng.png"
           alt="Briefly Logo"
-          className="w-64 mb-8 animate-pulse hover:drop-shadow-[0_0_4rem_#fff9559e] transition duration-300"
+          className="w-64 mb-8 animate-rotate-slow hover:drop-shadow-[0_0_4rem_rgba(255,249,85,0.6)] transition duration-300"
         />
 
         <p className="text-xl md:text-lg font-bold mb-2">
@@ -27,12 +27,14 @@ function App() {
           <br />
           Stay tuned for updates.
         </p>
-        <div className="flex gap-10">
+        <div className="flex gap-10 ">
           <a
             href="https://github.com/khizar-45"
             target="_blank"
             rel="noopener noreferrer"
-            className="px-4 py-2 bg-[hsl(56,100%,50%)] text-black rounded-lg font-semibold hover:brightness-125 transition"
+            className="px-4 py-2 bg-black text-white rounded-lg font-semibold shadow-[inset_0_0_0_1px_white]
+            active:bg-[hsl(56,100%,50%)] active:text-black active:scale-95
+            hover:shadow-none hover:bg-[hsl(56,100%,50%)] hover:text-black transition-all duration-50 w-26"
           >
             GitHub
           </a>
@@ -40,7 +42,9 @@ function App() {
             href="https://linkedin.com/in/khizar45"
             target="_blank"
             rel="noopener noreferrer"
-            className="px-4 py-2 bg-[hsl(56,100%,50%)] text-black rounded-lg font-semibold hover:brightness-125 transition"
+            className="px-4 py-2 bg-black text-white rounded-lg font-semibold shadow-[inset_0_0_0_1px_white]
+            active:bg-[hsl(56,100%,50%)] active:text-black active:scale-95
+            hover:shadow-none hover:bg-[hsl(56,100%,50%)] hover:text-black transition-all duration-50 w-26"
           >
             LinkedIn
           </a>
