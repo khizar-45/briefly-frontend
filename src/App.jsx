ce585c8aa6ddb43b2c0b676255b79cb218852c6b
khizar-45 <skkhizarali45@gmail.com>
2025-10-08 17:05:09 +0530

    added vercel's speed insights package

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Homepage from "./pages/Home";
 import LandingPage from "./pages/LandingPage";
 import { Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <>
       <Analytics />
+      <SpeedInsights />
 
     <Routes>
       <Route path="/" element={<LandingPage />} />
